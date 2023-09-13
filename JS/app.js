3c5cb63e8d1090785fde12925f8b4a94b2368801
Tom <capps14e@gmail.com>
2023-09-13 15:41:35 -0400

    added and formatted questions for quiz

--- JS/app.js
+++ JS/app.js
@@ -10,7 +10,7 @@ if (cleanUserAnswer === 'yes' || cleanUserAnswer === 'y') {
   alert('False!');
 }
 
-let militaryAnswer = prompt('Was I in military?? Please respond with yes or no!');
+let militaryAnswer = prompt('Was I in military? Please respond with yes or no!');
 
 let cleanmilitaryAnswer = militaryAnswer.trim().toLowerCase();
 
@@ -20,3 +20,12 @@ if (cleanmilitaryAnswer === 'yes' || cleanmilitaryAnswer === 'y') {
   alert('False!');
 }
 
+let militaryAnswer = prompt('Was I in military?? Please respond with yes or no!');
+
+let cleanmilitaryAnswer = militaryAnswer.trim().toLowerCase();
+
+if (cleanmilitaryAnswer === 'yes' || cleanmilitaryAnswer === 'y') {
+  alert('Correct!');
+} else {
+  alert('False!');
+}
