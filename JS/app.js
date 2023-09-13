4b04d7527179f0dcb5d8f1d874e122061c3271be
Tom <capps14e@gmail.com>
2023-09-13 15:41:48 -0400

    formatting/spelling

--- JS/app.js
+++ JS/app.js
@@ -20,12 +20,33 @@ if (cleanmilitaryAnswer === 'yes' || cleanmilitaryAnswer === 'y') {
   alert('False!');
 }
 
-let militaryAnswer = prompt('Was I in military?? Please respond with yes or no!');
+let PCAnswer = prompt('Have I built a Mac? Please respond with yes or no!');
 
-let cleanmilitaryAnswer = militaryAnswer.trim().toLowerCase();
+let cleanPCAnswer = PCAnswer.trim().toLowerCase();
 
-if (cleanmilitaryAnswer === 'yes' || cleanmilitaryAnswer === 'y') {
+if (cleanPCAnswer === 'no' || cleanPCAnswer === 'n') {
   alert('Correct!');
 } else {
   alert('False!');
 }
+
+let PowderAnswer = prompt('Was I a powdercoater? Please respond with yes or no!');
+
+let cleanPowderAnswer = PowderAnswer.trim().toLowerCase();
+
+if (cleanPCAnswer === 'yes' || cleanPowderAnswer === 'y') {
+  alert('Correct!');
+} else {
+  alert('False!');
+}
+
+let shredAnswer = prompt('Do I absolutely shred on guitar? Please respond with yes or no!');
+
+let cleanshredAnswer = shredAnswer.trim().toLowerCase();
+
+if (cleanUserAnswer === 'yes' || cleanshredAnswer === 'y') {
+  alert('Correct!');
+} else {
+  alert('False!');
+}
+
